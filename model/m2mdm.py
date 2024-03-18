8755f2e9b381eb23f982fee5eb9183ab74fb5d51
Orr Avrech <orravrech@gmail.com>
2024-03-18 14:49:27 -0400

    add null-cond architecture

--- model/m2mdm.py
+++ model/m2mdm.py
@@ -118,6 +118,67 @@ class EncoderM2MDM(BaseM2MDM):
         output = self.output_process(output)  # [bs, njoints, nfeats, nframes]
         return output
 
+
+class NullCondEncoderM2MDM(BaseM2MDM):
+    def __init__(self, njoints, nfeats, seq_len, latent_dim=256, ff_size=1024, num_layers=8, 
+                 num_heads=4, dropout=0.1, activation="gelu", data_rep='rot6d', 
+                 cond_dim=263, arch='trans_enc', emb_trans_dec=False):
+        super().__init__(njoints, nfeats, latent_dim, ff_size, num_layers, 
+                         num_heads, dropout, activation, data_rep, 
+                         cond_dim, arch, emb_trans_dec)
+        self.seq_len = seq_len
+        self.null_cond_embed = nn.Parameter(torch.randn(self.seq_len, 1, self.latent_dim))
+        self.seqTransEncoder = nn.TransformerEncoder(self.seqTransEncoderLayer,
+                                                     num_layers=self.num_layers)
+        
+    def mask_cond(self, cond: Tensor, cond_mask_prob: float):
+        # return a keep-mask as follows: 1-> real-cond, 0-> null-cond, by applying:
+        # cond_tokens = torch.where(keep_mask, cond_tokens, null_cond_embed)
+        # cond_mask_prob == 1 means that the null-cond is always chosen (uncond)
+        # cond_mask_prob == 0 means that the real-cond is always chosen (unguided)
+        bs = cond.shape[1]
+        if cond_mask_prob == 1:
+            keep_mask =  torch.zeros(bs, dtype=torch.bool)
+        elif cond_mask_prob == 0:
+            keep_mask = torch.ones(bs, dtype=torch.bool)
+        else:
+            keep_mask = torch.zeros(bs).float().uniform_(0, 1) > cond_mask_prob
+        keep_mask = keep_mask.unsqueeze(-1).unsqueeze(0)
+        keep_mask = keep_mask.to(cond.device)
+        return keep_mask    
+    
+    def forward(self, x: Tensor, timesteps: Tensor, y: Dict, cond_mask_prob: float):
+        """
+        x: [batch_size, njoints, nfeats, max_frames], denoted x_t in the paper
+        timesteps: [batch_size] (int)
+        """
+        # timestemp MLP
+        emb = self.embed_timestep(timesteps)  # [1, bs, d]
+
+        # encode cond
+        cond_embed = y["motion"]
+        cond_tokens = self.cond_projection(cond_embed)
+        cond_tokens = self.sequence_pos_encoder(cond_tokens)
+        cond_tokens = self.cond_encoder(cond_tokens)
+
+        # mask and project cond
+        null_cond_embed = self.null_cond_embed.to(cond_tokens.dtype)
+        keep_mask = self.mask_cond(cond_tokens, cond_mask_prob)
+        cond_mask = torch.where(keep_mask, cond_tokens, null_cond_embed)
+        cond_mask_pooled = cond_mask.mean(dim=0).unsqueeze(0)
+        emb += self.embed_motion(cond_mask_pooled)
+
+        x = self.input_process(x)
+
+        # adding the cond + t embed
+        xseq = torch.cat((emb, x), axis=0)  # [seqlen+1, bs, d]
+        xseq = self.sequence_pos_encoder(xseq)  # [seqlen+1, bs, d]
+        # encoder-only
+        output = self.seqTransEncoder(xseq)[1:]  # , src_key_padding_mask=~maskseq)  # [seqlen, bs, d]
+
+        output = self.output_process(output)  # [bs, njoints, nfeats, nframes]
+        return output
+
         
 class DecoderM2MDM(BaseM2MDM):
     def __init__(self, njoints, nfeats, latent_dim=256, ff_size=1024, num_layers=8, 
