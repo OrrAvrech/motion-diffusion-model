aa242338a0d57945a34ac4e0399d45f95bb26001
Orr Avrech <orravrech@gmail.com>
2024-04-11 03:10:20 -0400

    prepare to split_motions for MotionX

--- data_loaders/humanml/scripts/split_motions.py
+++ data_loaders/humanml/scripts/split_motions.py
@@ -7,13 +7,13 @@ def main():
     subsample_factor = 2
     num_frames = int(196*subsample_factor)
 
-    dataset_dir = Path("/proj/vondrick2/orr/motion-diffusion-model/dataset/MOYO")
-    vecs_dir = dataset_dir / "new_joint_vecs"
+    dataset_dir = Path("/proj/vondrick2/orr/motion-diffusion-model/dataset/MotionX")
+    vecs_dir = dataset_dir / "vector_263"
     output_dir = dataset_dir / "human_feedback" / "new_joint_vecs_split"
     output_dir.mkdir(exist_ok=True, parents=True)
-    text_dir = dataset_dir / "texts"
-    new_text_dir = dataset_dir / "texts_split"
-    new_text_dir.mkdir(exist_ok=True, parents=True)
+    # text_dir = dataset_dir / "texts"
+    # new_text_dir = dataset_dir / "texts_split"
+    # new_text_dir.mkdir(exist_ok=True, parents=True)
     split_filepath = dataset_dir / "vecs_12.txt"
 
     filenames = []
@@ -21,17 +21,17 @@ def main():
         full_motion = np.load(npy_path)
         for i in range(0, len(full_motion) - num_frames, num_frames):
             curr_motion = full_motion[i: i+num_frames]
-            # MOYO is in 40fps, HumanML3D 20fps
+            # MOYO is in 40fps, HumanML3D 20fps, MotionX 30fps
             curr_motion = curr_motion[::subsample_factor]
             if len(curr_motion) <= 60:
-                # skip short motions
+                 # skip short motions
                 continue
 
             motion_filename = f"{npy_path.stem}_{i}_{i+num_frames}.npy"
             filenames.append(motion_filename)
             np.save(output_dir / motion_filename, curr_motion)
 
-            copy(text_dir / f"{npy_path.stem}.txt", new_text_dir / f"{Path(motion_filename).stem}.txt")
+            # copy(text_dir / f"{npy_path.stem}.txt", new_text_dir / f"{Path(motion_filename).stem}.txt")
     
     with open(split_filepath, "a") as fp:
         [fp.write(f"{x}\n") for x in filenames]
